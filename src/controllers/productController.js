f595a4f8a29d70b141fca099df9d382a3cda678a
jefti <jeftimeira@gmail.com>
2023-07-14 18:13:00 -0300

    feat/ retorna produto por categoria

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,4 +1,5 @@
 import { db } from "../database/database.js";
+import { ObjectId } from "mongodb";
 
 export async function cadatraProduto(req,res){
     const produto = req.body;
@@ -16,7 +17,7 @@ export async function cadatraProduto(req,res){
 export async function retornaTodosProdutos(req, res){
     try{
         const lista = await db.collection("produtos").find({}).toArray();
-        res.send(lista);
+        res.send(lista) ;
     }catch(err){
         return res.status(500).send(err.message);
     }
@@ -25,12 +26,25 @@ export async function retornaTodosProdutos(req, res){
 export async function retornaCategoriaProdutos(req, res){
     try{
         const categoria = req.params.categoria;
+        if(!categoria) return res.status(401).send('id não encontrado na requisição.');
         const lstCategorias = ['quadro', 'ilustracao','escultura'];
         if(!lstCategorias.includes(categoria)) return res.status(404).send('categoria não encontrada.');
         const lista = await db.collection("produtos").find({categoria}).toArray();
         return res.send(lista);
-        
+
         }catch(err){
+            return res.status(500).send(err.message);
+    }
+}
+
+export async function retornaProduto(req, res){
+    try{
+        const id = new ObjectId(req.params.id);
+        if(!id) return res.status(401).send('id não encontrado na requisição.');
+        const produto = await db.collection("produtos").findOne({_id:id});
+        if(!produto) return res.status(404).send('Não foi encontrado nenhum produto com o ID informado.');
+        return res.send(produto);
+    }catch(err){
             return res.status(500).send(err.message);
     }
 }
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { productSchemaValidation } from "../middlewares/productSchemaValidationMiddleware.js";
-import { cadatraProduto, retornaTodosProdutos, retornaCategoriaProdutos } from "../controllers/productController.js";
+import { cadatraProduto, retornaTodosProdutos, retornaCategoriaProdutos, retornaProduto } from "../controllers/productController.js";
 import { tokenValidate } from "../middlewares/tokenValidationMiddleware.js";
 
 const productRouter = Router();
@@ -8,5 +8,6 @@ const productRouter = Router();
 productRouter.post("/produto",productSchemaValidation,cadatraProduto)
 productRouter.get("/produtos/todos", tokenValidate, retornaTodosProdutos)
 productRouter.get("/categoria/:categoria", tokenValidate, retornaCategoriaProdutos)
+productRouter.get("/produto/:id", tokenValidate, retornaProduto)
 
 export default productRouter
