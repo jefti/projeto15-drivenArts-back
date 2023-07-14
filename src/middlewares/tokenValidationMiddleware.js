e1e343bba16aed4ddcb67409b614d83d28fcca99
jefti <jeftimeira@gmail.com>
2023-07-14 17:21:10 -0300

    fix: hearder para headers

--- src/middlewares/tokenValidationMiddleware.js
+++ src/middlewares/tokenValidationMiddleware.js
@@ -1,7 +1,7 @@
 import { db } from "../database/database.js ";
 
 export async function tokenValidate(req, res, next){
-    const { authorization } = req.header;
+    const { authorization } = req.headers;
     const token = authorization?.replace('Bearer ', '');
 
     if(!token) return res.status(401).send('Token não encontrado na requisição.');
@@ -10,6 +10,5 @@ export async function tokenValidate(req, res, next){
     if (!session) return res.status(401).send('Token de login não encontrado no sistema.');
 
     res.locals.id_usuario = session.id_usuario;
-    
     next();
 }
